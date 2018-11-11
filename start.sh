09ca236089e84b4b95d46a0875adfc6e86006de7
Jiapengcs <jiapengkang@gmail.com>
2018-11-11 18:00:29 +0800

    modify start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 
 # app name, must be modified!(used for directory name, so it cannot contains special symbols)
 APP_NAME=blog-alpha
-TAR_NAME=artifacts.tar.gz
+ARTIFACTS_NAME=artifacts.tar.gz
 BASE_PATH=~/deploy/$APP_NAME
-BIN_PATH=$BASE_PATH/bin
+TARGET_PATH=$BASE_PATH/target
 
 echo "=== Start Deploying... ==="
 
@@ -22,14 +22,14 @@ fi
 
 
 # clean and extract files
-if [ -d "/${BIN_NAME}" ]
+if [ -d "${TARGET_PATH}" ]
 then
-    echo "directory ${BIN_PATH} already exists, cleaning..."
-    rm -r $BIN_PATH
+    echo "directory ${TARGET_PATH} already exists, cleaning..."
+    rm -r $TARGET_PATH
 fi
 
-echo "Extracting files to ${BIN_PATH}..."
-tar -zxvf $TAR_NAME -C $BIN_PATH
+echo "Extracting files..."
+tar -zxvf $ARTIFACTS_NAME
 if [ $? != 0 ]
 then
     echo "extract file failed!"
@@ -39,8 +39,9 @@ fi
 
 
 # run new instance
+cd $TARGET_PATH
 echo "Running new instance..."
-nohup java -jar $BIN_PATH/*.jar > logs.txt & echo $! > pid.txt
+nohup java -jar *.jar > logs.txt & echo $! > pid.txt
 echo "appending logs to logs.txt, and write pid to pid.txt..."
 sleep 1
 
